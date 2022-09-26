043b1f248f7259a67a8d8d92af34f3f8a7133d7c
dluca22 <luc.due@tim.it>
2022-09-26 21:27:27 +0200

    added gitignore and virtualenv

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name='wiki'
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("random", views.random, name="random"),
-    path("<str:entry>/", views.entry, name="entry"),
-    path("create", views.create, name="create"),
+    path('', views.index, name='index'),
+    path('random', views.random, name='random'),
+    path('create', views.create, name='create'),
+    path('<str:title>', views.entry, name='title'),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,9 @@
+from audioop import reverse
 from http.client import HTTPResponse
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, redirect
 from random import choice
+from markdown2 import Markdown
 
 from . import util
 
@@ -11,15 +13,25 @@ def index(request):
         "entries": util.list_entries()
     })
 
-def entry(request, entry):
-    article = util.get_entry(entry)
+def entry(request, title):
+    article = util.get_entry(title)
+    if article is None:
+        return render(request, "encyclopedia/notExisting.html", {'article': title})
+
     # convertire a html
-    return render(request, "encyclopedia/article.html", {'article': article})
+    else:
+        markdowner = Markdown()
+        return render(request, "encyclopedia/article.html", {'article': markdowner.convert(article), 'title': title})
 
 def create(request):
-    return render(request, "encyclopedia/create.html")
+    var = "culo"
+    return render(request, "encyclopedia/create.html", {'var': var})
 
 def random(request):
     random = choice(util.list_entries())
-    article = util.get_entry(random)
-    return redirect(request, "encyclopedia/article.html", {'article': article})
+    random_article = util.get_entry(random)
+    return HttpResponseRedirect(reverse('title', kwargs = {'title': random_article}))
+
+    # NO return render(request, "encyclopedia/article.html", {'article': article})
+
+    # NO return redirect(request, f"encyclopedia/{article}.html", {'article': article})
